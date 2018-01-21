29da057d4dd0d668ad26af29a091188b400cc9e1
JonasKoppen_DROS <jonas.koppen@student.ap.be>
2018-01-21 14:54:38 +0100

    update

--- app/src/main/java/koppen/jonas/ap/projectbig/ChatApp/MainActivity.java
+++ app/src/main/java/koppen/jonas/ap/projectbig/ChatApp/MainActivity.java
@@ -2,6 +2,9 @@ package koppen.jonas.ap.projectbig.ChatApp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,11 +13,20 @@ import koppen.jonas.ap.projectbig.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
+        editInput = findViewById(R.id.editInputChat);
         setValue("hi");
+        ((Button)findViewById(R.id.btnEnterChat)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setValue(editInput.getText().toString());
+                editInput.setText("");
+            }
+        });
     }
 
     public void setValue(String value){
@@ -22,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        myRef.setValue(value);
+
+    }
+
+    public void getValue(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
     }
 }
