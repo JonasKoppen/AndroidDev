b6e139f226bf2dacb8432bb0b65d9c67d0e598be
JonasKoppen_DROS <jonas.koppen@student.ap.be>
2018-01-22 10:19:23 +0100

    Random Background thread in storrage

--- app/src/main/java/koppen/jonas/ap/projectbig/storrage/MainActivity.java
+++ app/src/main/java/koppen/jonas/ap/projectbig/storrage/MainActivity.java
@@ -1,8 +1,10 @@
 package koppen.jonas.ap.projectbig.storrage;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,6 +12,10 @@ import android.widget.Toast;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 import javax.annotation.Nullable;
 
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
     Realm realm;
     WhishList wishes;
+    Thread t;
+    Context hi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,44 @@ public class MainActivity extends AppCompatActivity {
         wishes = new WhishList();
         setContentView(R.layout.activity_main_storrage);
         wishList = (EditText) findViewById(R.id.editWishList);
+        this.hi = this;
 
         loadList(findViewById(R.id.btnSave));
-
+        t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String[] items = wishList.getText().toString().split("\n");
+                for (int i = 0; i < 100; i++) {
+                    List<String> listItems = Arrays.asList(items);
+                    Collections.shuffle(listItems);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    final int j = i; // Well this is ugly
+                    runOnUiThread(new Runnable() { // And this
+                        public void run() {
+                            wishList.setText(j + " %");
+                        }
+                    });
+                    String[] shuffledItems = new String[listItems.size()];
+                    listItems.toArray(shuffledItems);
+                    String progress = TextUtils.join("\n", shuffledItems);
+                    // TODO publish progress
+                    items = shuffledItems;
+                }
+                final String[] result = items;
+                runOnUiThread(new Runnable() { // And this
+                    public void run() {
+                        Toast toast = Toast.makeText(hi, "Done", Toast.LENGTH_SHORT);
+                        toast.show();
+                        wishList.setText(TextUtils.join("\n", result));
+                        saveListFile(TextUtils.join("\n", result));
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -44,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveList(View v){
-        saveListFile();
+        saveListFile(wishList.getText().toString());
     }
 
     public void saveListPref(){
@@ -54,11 +97,11 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
     }
 
-    public void saveListFile(){
+    public void saveListFile(String input){
         FileOutputStream fos = null;
         try{
             fos = openFileOutput("wishlist", MODE_PRIVATE);
-            fos.write(wishList.getText().toString().getBytes());
+            fos.write(input.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
             Toast.makeText(MainActivity.this,"save error: fnf",Toast.LENGTH_LONG).show();
@@ -105,4 +148,8 @@ public class MainActivity extends AppCompatActivity {
         wishList.setText(tekst);
     }
 
+
+    public void randomList(View v){
+        t.start();
+    }
 }
